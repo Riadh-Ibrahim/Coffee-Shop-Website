b8245131a03bbdb45d59bfc1a9f78760e3d79913
E-Miraoui <122991654+E-Miraoui@users.noreply.github.com>
2023-04-29 14:43:22 +0100

    Add files via upload

---
+++ SignUpbutton.js
@@ -0,0 +1,12 @@
+let sign1=document.getElementById('join');
+
+
+sign1.addEventListener('click', function(){
+    window.location.href="http://localhost/web/";
+})
+
+let globe=document.getElementById('lang');
+
+globe.addEventListener('click', function(){
+    window.location.href="../traduction/traduction.html";
+})
